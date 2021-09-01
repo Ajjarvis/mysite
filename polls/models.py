83bb5660096ba9f754e7400769b25376746f75a5
ajc-odoo <ajc@odoo.com>
2021-09-01 09:55:42 +0530

    Completed the django polls app tutorial

--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.db.models.fields import DateField
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Question(models.Model):
@@ -12,8 +13,14 @@ class Question(models.Model):
     def __str__(self):
         return str(self.id) + "-" + self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published reecently?'
+    )
     def was_published_recently(self):
-        return self.pub_date >= (timezone.now() - datetime.timedelta(days=1))
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
